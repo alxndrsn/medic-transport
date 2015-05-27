aa849206d293024a2072bc66887f532bb72cd031
Alex Anderson <anderson@medicmobile.org>
2015-05-27 18:37:40 +0200

    whitespace cleanup

--- test/adaptor/medic-mobile-test.js
+++ test/adaptor/medic-mobile-test.js
@@ -7,7 +7,8 @@ describe('medic-mobile', function() {
   var TEST_MESSAGE = {content:'', from:'', timestamp:''},
       TEST_URL_ROOT = 'http://localhost/nonsense'
       _json = JSON.stringify,
-      mm = adaptor.create('medic-mobile', {debug:true, pass:'secret', url:TEST_URL_ROOT});
+      mm = adaptor.create('medic-mobile',
+         {debug:true, pass:'secret', url:TEST_URL_ROOT});
 
   before(function(done) {
     sinon.stub(request, 'get');
@@ -50,7 +51,7 @@ describe('medic-mobile', function() {
         assert.equal(args.length, 2);
         assert.equal(args[0].url, TEST_URL_ROOT + '/add');
 
-	// FIXME at the moment, we appear to make two identical calls to '/add'
+        // FIXME at the moment, we appear to make two identical calls to '/add'
         assert.equal(request.post.callCount, 2);
         assert.deepEqual(request.post.firstCall.args[0], request.post.secondCall.args[0]);
 
